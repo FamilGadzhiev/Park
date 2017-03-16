7be9af1157a9ca2212a8b5b88a0a4520ca98e2fd
famil <famil951214@yandex.ru>
2017-03-16 22:24:32 +0300

    Add keyBoardShow/Hide

--- Avionicus/LoginVeiwController.swift
+++ Avionicus/LoginVeiwController.swift
@@ -16,34 +16,29 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var loginEnter: RoundTextField!
     @IBOutlet weak var passwordEnter: RoundTextField!
     @IBOutlet weak var signIn: RoundButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loginEnter.delegate = self
+        registerForKeyboardNotification()
         
-        //let request = Avionicus.registration("famil", "12332", "famil@yandex.ru").request
-        //print (request)
+    }
+    deinit {
+        removeKeyboardNotification()
     }
     
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        let text = (loginEnter.text! as NSString).replacingCharacters(in: range, with: string)
-        
-        if !text.isEmpty{
-            signIn.isUserInteractionEnabled = true
-        } else {
-            signIn.isUserInteractionEnabled = true
-        } 
-        return true
+    @IBAction func topText(_ sender: Any) {
+        print("Touch")
     }
-  
     
+
     @IBAction func loginAction(_ sender: AnyObject) {
         
-        let loginInput = self.loginEnter.text!
-        let password = self.passwordEnter.text!
+        let loginInput = "famil"//self.loginEnter.text!
+        let password = "12786978"//self.passwordEnter.text!
         
         let md5s2: Digest = Digest(algorithm: .md5)
         md5s2.update(string: password)
@@ -71,5 +66,39 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
                 }
             }
         }
+    }
+    
+    func registerForKeyboardNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+         NotificationCenter.default.addObserver(self, selector: #selector(kbWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    func removeKeyboardNotification()  {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+    }
+    
+    func kbWillShow(_ notifitacion: Notification) {
+        let userInfo = notifitacion.userInfo
+        let kbFrame = (userInfo?[UIKeyboardFrameEndUserInfoKey] as! NSValue ).cgRectValue
+        scrollView.contentOffset = CGPoint(x: 0, y: 100)
+        
+        
+    }
+    func kbWillHide() {
+        scrollView.contentOffset = CGPoint.zero
+    }
+
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let text = (loginEnter.text! as NSString).replacingCharacters(in: range, with: string)
+        
+        if !text.isEmpty{
+            signIn.isUserInteractionEnabled = true
+        } else {
+            signIn.isUserInteractionEnabled = true
+        }
+        return true
     }
 }
